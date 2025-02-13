0fc1d4909f949808f3cafa9b58e711548693d4c8
Gustavo Sales <gus@codegus.com>
2025-02-13 10:49:18 -0300

    feat: default structure

---
+++ src/app/_components/users.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Users() {
+  return (
+    <div>
+      <button>Novo Usuário</button>
+      <hr className="my-3" />
+      <ul>
+        <li>Nome / Email</li>
+      </ul>
+    </div>
+  );
+}
